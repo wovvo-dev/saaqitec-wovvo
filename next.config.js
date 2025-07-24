4a00e19c9dc9d2ff92e7da5c7b86a7df18eb72d4
developersaaqi-cmd <developersaaqi@gmail.com>
2025-07-24 06:29:59 +0500

    Updated next.config.js to include GHL environment variables

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_GHL_API_BASE_URL: process.env.NEXT_PUBLIC_GHL_API_BASE_URL,
+    NEXT_PUBLIC_GHL_API_AUTH_KEY: process.env.NEXT_PUBLIC_GHL_API_AUTH_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY
+  }
+}
 
 module.exports = nextConfig
