f3bf6e638e79276457b673a7f1da41be015c4896
developersaaqi-cmd <developersaaqi@gmail.com>
2025-07-24 05:48:40 +0500

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
+// src/app/layout.tsx
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { GoogleAnalytics } from "@next/third-parties/google";
-// import { GoogleAnalytics ,usePageViews } from "nextjs-google-analytics";
+import Script from "next/script";
+
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import FooterSection from "../components/FooterSection";
 import UserConsentModal from "../components/ui/user-consent-modal";
 import TermsAndConditionsModal from "../components/ui/terms-conditions-modal";
-import Script from 'next/script';
 import { ReferralProvider } from "../context/ReferralContext";
 
 const geistSans = localFont({
@@ -38,41 +39,42 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  //  usePageViews(); 
+}) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <head>
+        {/* Twitter Pixel */}
         <Script id="twitter-conversion-tracking" strategy="afterInteractive">
           {`!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){
-          s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-          },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,
-          u.src='https://static.ads-twitter.com/uwt.js',
-          a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-          twq('config','pks6f');`}
+              s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+              },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,
+              u.src='https://static.ads-twitter.com/uwt.js',
+              a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+              twq('config','pks6f');`}
         </Script>
-
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <Navbar />
-        <ReferralProvider>
-          {children}
-        </ReferralProvider>
-
+        <ReferralProvider>{children}</ReferralProvider>
         <FooterSection />
         <UserConsentModal />
         <TermsAndConditionsModal />
 
-        <Script src="https://r.wdfl.co/rw.js" data-rewardful="fb0572"></Script>
+        {/* Rewardful */}
+        <Script
+          src="https://r.wdfl.co/rw.js"
+          data-rewardful="fb0572"
+          strategy="afterInteractive"
+        />
         <Script id="rewardful-queue" strategy="beforeInteractive">
           {`(function(w,r){w._rwq=r;w[r]=w[r]||function(){(w[r].q=w[r].q||[]).push(arguments)}})(window,'rewardful');`}
         </Script>
+
+        {/* Google Analytics */}
+        <GoogleAnalytics gaId="G-HD79YKJYKD" />
       </body>
-      <GoogleAnalytics gaId="G-HD79YKJYKD" />
     </html>
   );
 }
